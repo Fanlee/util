d6bab3bd54895abdb93e063b2340955c9a0ab3f0
Fanlee <anyueleo@163.com>
2018-04-13 14:47:28 +0800

    增加数据类型判断，是否是类数组等方法

--- base.js
+++ base.js
@@ -117,5 +117,72 @@ var Base = {
     }
 
     return typeof obj === 'object' || typeof obj === 'function' ? classType[Object.prototype.toString.call(obj)] || 'object' : typeof obj
+  },
+  /**
+   * 判断是否是纯粹的对象
+   */
+  isPlainObject: function(obj) {
+    var proto, ctor
+    var classType = {}
+
+    // 相当于Object.prototype.toString
+    var toString = classType.toString
+
+    // 相当于Object.prototype.hasOwnProperty
+    var hasOwn = classType.hasOwnProperty
+
+    if (!obj || toString.call(obj) !== '[object Object]') {
+      return false
+    }
+
+    proto = Object.getPrototypeOf(obj)
+
+    // 没有原型的对象是纯粹的，Object.create(null) 就在这里返回 true
+    if (!proto) {
+      return true
+    }
+
+    /**
+     * 以下判断通过 new Object 方式创建的对象
+     * 判断 proto 是否有 constructor 属性，如果有就让 Ctor 的值为 proto.constructor
+     * 如果是 Object 函数创建的对象，Ctor 在这里就等于 Object 构造函数
+     */
+    ctor = hasOwn.call(proto, 'constructor') && proto.constructor
+
+    // 在这里判断 Ctor 构造函数是不是 Object 构造函数，用于区分自定义构造函数和 Object 构造函数
+    return typeof ctor === 'function' && hasOwn.toString.call(ctor) === hasOwn.toString.call(Object)
+
+  },
+  /**
+   * 判断是否是空的对象
+   *    for循环一旦执行就说明有属性
+   * @param {Object} 传入对象
+   */
+  isEmptyObject: function(obj) {
+    var name
+    for (name in obj) {
+      return false
+    }
+    return true
+  },
+  /**
+   * 判断是否有全局的window对象
+   */
+  isWindow: function(obj) {
+    return obj != null && obj.window
+  },
+  /**
+   * 判断是否是数组或者类数组
+   */
+  isArrayLike: function(obj) {
+    var length = !!obj && 'length' in obj && obj.length
+    var typeRes = this.type(obj)
+
+    // 排除掉函数和window对象
+    if (typeRes === 'function' || this.isWindow(obj)) {
+      return false
+    }
+
+    return typeRes === 'array' || length === 0 || typeof length === 'number' && length > 0 && (length - 1) in obj
   }
 }

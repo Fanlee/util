4d292bfb7379111e07e802b9c85c13755f81837e
Fanlee <anyueleo@163.com>
2018-04-16 14:18:29 +0800

    add extend

--- base.js
+++ base.js
@@ -237,5 +237,58 @@ var Base = {
       }
     }
     return newObj
+  },
+  /**
+   * extend
+   */
+  extend: function() {
+    // 默认不进行深拷贝
+    var deep = false
+    var name, options, src, copy, clone, copyIsArray
+    var len = arguments.length
+    var i = 1
+    // 第一个参数不传布尔值的情况下，target默认是第一个参数
+    var target = arguments[0] || {}
+    // 如果第一个参数是布尔值，target为第二个参数
+    if (typeof target === 'boolean') {
+      deep = target
+      target = arguments[i] || {}
+      i++
+    }
+    // 如果target不是对象，无法进行复制，所以设为{}
+    if (typeof target !== 'object' && !this.isFunction(target)) {
+      target = {}
+    }
+
+    for (; i < len; i++) {
+      options = arguments[i]
+      if (options != null) {
+        for (name in options) {
+          // 目标属性值
+          src = target[name]
+          // 要复制的对象的属性值
+          copy = options[name]
+          // 解决循环引用
+          if (target === copy) {
+            continue
+          }
+          // 要递归的对象必须是plainObject或者数组
+          if (deep && copy && (this.isPlainObject(copy) || (copyIsArray = Array.isArray(copy)))) {
+            // 要复制的对象属性值类型需要与目标属性值相同
+            if (copyIsArray) {
+              copyIsArray = false
+              clone = src && Array.isArray(src) ? src : []
+            } else {
+              clone = src && this.isPlainObject(src) ? src : {}
+            }
+
+            target[name] = this.extend(deep, clone, copy)
+          } else if (copy !== undefined) {
+            target[name] = copy
+          }
+        }
+      }
+    }
+    return target
   }
 }
